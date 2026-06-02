c0d71d97fae1ca0881f1d6cd0cdd0ec71b639a6c
GIvGI <giorgikhuroshvili8@gmail.com>
2026-06-03 00:14:35 +0400

    add language content, fix js fragment

--- js/script.js
+++ js/script.js
@@ -4,13 +4,9 @@ div.innerHTML = '<h2 id="introductions">VEUILLEZ CHOISIR UN SUJET</h2>';
 const target = document.querySelector(".details-for-every-topic");
 target.prepend(div);
 
-if(window.location.href.includes('#')){
-    div.style.display = "none";
-}
-
-addEventListener("click", function(event){
-    const link = event.target.closest('a');
-    if(link){
-        div.style.display = "none";
-    }
-});
+const updateIntroductions = () => {
+    if (window.location.hash) {div.style.display = "none";}
+    else {div.style.display = "block";}
+};
+updateIntroductions();
+window.addEventListener("hashchange", updateIntroductions);
